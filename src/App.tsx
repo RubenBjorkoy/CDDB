1e611c82b4d4e89ca11373bbc77908d6f61b7cda
Ruben Vega Bjørkøy <ruben.bjorkoy@gmail.com>
2025-11-18 21:06:28 +0100

    react: cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <div>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </div>
       </header>
     </div>
   );
