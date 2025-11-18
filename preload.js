6b2df822c71282c71713dbfb7764fac2722615b0
Ruben Vega Bjørkøy <ruben.bjorkoy@gmail.com>
2025-11-18 20:57:34 +0100

    Started switch to react

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  executeQuery: (query) => ipcRenderer.invoke('get-cds', query),
-  addCD: (cd) => ipcRenderer.invoke('add-cd', cd),
-  updateCD: (cd) => ipcRenderer.invoke('update-cd', cd),
-  deleteCD: (id) => ipcRenderer.invoke('delete-cd', id),
-  getEnv: (key) => ipcRenderer.invoke('get-env', key),
-});
